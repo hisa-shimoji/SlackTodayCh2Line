2e82b4acf18ac93e8585809db6b5e72d1f6094d2
hisa-shimoji <hshimoji1@mac.com>
2021-01-21 16:23:04 +0900

    fix

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ const bot_token = process.env.SLK_BOT_TOKEN;
 const sign_secret = process.env.SLK_BOT_SIGN_SECRET;
 const monitor_channel = process.env.SLK_BOT_MONITOR_CHANNEL;
 
-// トリガーを引くときの設定キー
-const api_key = process.env.SLK_TODAY_CH2LINE_API_KEY;
-
 // LINEメッセージの前と後に付加する文字列 改行は環境変数では\\nで設定
-const line_msg_prefix = process.env.L_MSG_PREFIX.replace(/\\n/g, '\n');;
-const line_msg_suffix = process.env.L_MSG_SUFFIX.replace(/\\n/g, '\n');;
+const line_msg_prefix = process.env.L_MSG_PREFIX.replace(/\\n/g, '\n');
+const line_msg_suffix = process.env.L_MSG_SUFFIX.replace(/\\n/g, '\n');
 
 // slackでメッセージ収集を行うための初期化
 const app = new App({
@@ -20,65 +17,59 @@ const app = new App({
     signingSecret: sign_secret
 });
 
-
 exports.main = async (req, res) => {
 
     try {
-        if (api_key == req.body['key']) {
+        // 今日のJST日付0:00をUNIXtimeで取得
+        let utime = moment().tz("Asia/Tokyo").hour(0).minutes(0).second(0).format("X");
 
-            // 今日のJST日付0:00をUNIXtimeで取得
-            let utime = moment().tz("Asia/Tokyo").hour(0).minutes(0).second(0).format("X");
+        // slackメッセージ収集
+        const result = await app.client.conversations.history({
+            token: bot_token,
+            channel: monitor_channel,
+            limit: 20,
+            oldest: utime
+        });
 
-            // slackメッセージ収集
-            const result = await app.client.conversations.history({
-                token: bot_token,
-                channel: monitor_channel,
-                limit: 20,
-                oldest: utime
+        // slackユーザ名収集
+        const usr_list = await app.client.users.list({
+            token: bot_token
+        });
+        var usr_dict = {};
+        if (usr_list.ok) {
+            await usr_list.members.forEach((m) => {
+                usr_dict[m.id] = m.profile.display_name;
             });
-
-            // slackユーザ名収集
-            const usr_list = await app.client.users.list({
-                token: bot_token
-            });
-            var usr_dict = {};
-            if (usr_list.ok) {
-                await usr_list.members.forEach((m) => {
-                    usr_dict[m.id] = m.profile.display_name;
-                });
-            }
-
-            let l_msg = "";
-            // 送信メッセージ構築
-            if (result.ok && result.messages.length > 0) {
-                var ret = await result.messages.map((e) => {
-                    if (usr_dict[e.user]) {
-                        return `${usr_dict[e.user]}: ${e.text}`;
-                    }
-                });
-                l_msg = line_msg_prefix + ret.join('\n') + line_msg_suffix;
-            }
-
-            let ret_msg = "";
-            if (l_msg) {
-                // line notify 送信
-                await lineNotify.notify({
-                    message: l_msg,
-                });
-                let st_dt = moment.unix(utime);
-                ret_msg = '正常終了 LINEメッセージ送信しました 基準時:' + st_dt.toString();
-            } else {
-                ret_msg = '正常終了しましたが、LINEメッセージは送信されていません';
-            }
-
-            res.status(200).send(ret_msg);
-        } else {
-            res.sendStatus(403);
         }
+
+        let l_msg = "";
+        // 送信メッセージ構築
+        if (result.ok && result.messages.length > 0) {
+            var ret = await result.messages.map((e) => {
+                if (usr_dict[e.user]) {
+                    return `${usr_dict[e.user]}: ${e.text}`;
+                }
+            });
+            l_msg = line_msg_prefix + ret.join('\n') + line_msg_suffix;
+        }
+
+        let ret_msg = "";
+        if (l_msg) {
+            // line notify 送信
+            await lineNotify.notify({
+                message: l_msg,
+            });
+            let st_dt = moment.unix(utime);
+            ret_msg = '正常終了 LINEメッセージ送信しました 基準時:' + st_dt.toString();
+        } else {
+            ret_msg = '正常終了しましたが、LINEメッセージは送信されていません';
+        }
+
+        res.status(200).send(ret_msg);
 
     } catch (error) {
         console.error(error);
-        res.status(200).send('失敗しました');
+        res.status(200).send('処理失敗しました');
     }
 
 }
